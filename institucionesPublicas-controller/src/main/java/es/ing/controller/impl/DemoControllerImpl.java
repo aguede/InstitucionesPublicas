9367be255be266d7cba84f37ced59c5d488db450
alejandro.guede <alejandro.guede@aguede650w10.babel-si.local>
2019-10-09 08:43:23 +0200

    Se implementan cambios en los módulos.

--- institucionesPublicas-controller/src/main/java/es/ing/controller/impl/DemoControllerImpl.java
+++ institucionesPublicas-controller/src/main/java/es/ing/controller/impl/DemoControllerImpl.java
@@ -1,11 +1,9 @@
 package es.ing.controller.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import es.ing.controller.DemoController;
-import es.ing.services.DemoService;
 
 /**
  * Controller para pruebas de configuración. Implementa DemoController.
@@ -15,12 +13,13 @@ import es.ing.services.DemoService;
  */
 public class DemoControllerImpl implements DemoController {
 
-	@Autowired
-	private DemoService demoService;
+//	@Autowired
+//	private DemoService demoService;
 
 	@Override
 	public ResponseEntity<String> createAuthenticationToken() {
-		return new ResponseEntity<String>(demoService.getHelloWorld(), HttpStatus.OK);
+//		return new ResponseEntity<String>(demoService.getHelloWorld(), HttpStatus.OK);
+		return new ResponseEntity<String>("hello world", HttpStatus.OK);
 	}
 
 }
