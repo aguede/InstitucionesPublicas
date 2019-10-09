babcd1829316bfbce452d91e17004f31f196a9e2
alejandro.guede <alejandro.guede@aguede650w10.babel-si.local>
2019-10-09 10:29:42 +0200

    Se configura aplicación para arranque de SpringBoot.

---
+++ instituciones-boot/src/main/java/org/instituciones/boot/InstitucionesApplication.java
@@ -0,0 +1,13 @@
+package org.instituciones.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitucionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstitucionesApplication.class, args);
+    }
+
+}
