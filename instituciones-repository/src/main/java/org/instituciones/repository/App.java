c1fcd457928bbad16b15a142a0c80d028caf3a49
alejandro.guede <alejandro.guede@aguede650w10.babel-si.local>
2019-10-09 09:04:56 +0200

    Se actualiza estructura del proyecto.

---
+++ instituciones-repository/src/main/java/org/instituciones/repository/App.java
@@ -0,0 +1,13 @@
+package org.instituciones.repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
