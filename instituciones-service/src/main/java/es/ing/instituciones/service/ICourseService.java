594b9bfdc5fa5e4bdcecd0d10f24e474506bfce4
DanBabel <daniel.diaz@babel.es>
2019-10-16 10:23:43 +0200

    Correciones por Sonar, quitar la declaracion de la excepcion

--- instituciones-service/src/main/java/es/ing/instituciones/service/ICourseService.java
+++ instituciones-service/src/main/java/es/ing/instituciones/service/ICourseService.java
@@ -4,58 +4,66 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import es.ing.instituciones.bean.exception.InstitucionesPublicasServiceException;
 import es.ing.instituciones.bean.modelo.CourseBean;
 
 /**
  * Lógica de negocio referente a los Courses
- * 
+ *
  * @author daniel.diaz
  *
  */
 @Service
 public interface ICourseService {
 
-	/**
-	 * Crea un curso en la base de datos
-	 * 
-	 * @param course - curso a crear
-	 * @return curso - creado
-	 */
-	public CourseBean create(CourseBean course);
-	
-	/**
-	 * Actualiza un curso
-	 * 
-	 * @param id - identificador del curso a actualizar
-	 * @param course - información actualizada del curso
-	 * @return curso actualizado
-	 * @throws EntityNotFoundException Excepción en caso de no haber encontrado el curso a actualizar
-	 */
-	public CourseBean update(Long id, CourseBean course) throws InstitucionesPublicasServiceException;
+    /**
+     * Crea un curso en la base de datos
+     * 
+     * @param course
+     *            - curso a crear
+     * @return curso - creado
+     */
+    public CourseBean create(CourseBean course);
 
-	/**
-	 * Elimina un curso
-	 * 
-	 * @param id - identificación del curso a eliminar
-	 * @throws EntityNotFoundException Excepción en caso de no haber encontrado el curso a eliminar
-	 */
-	public void delete(Long id) throws InstitucionesPublicasServiceException;
+    /**
+     * Actualiza un curso
+     * 
+     * @param id
+     *            - identificador del curso a actualizar
+     * @param course
+     *            - información actualizada del curso
+     * @return curso actualizado
+     * @throws EntityNotFoundException
+     *             Excepción en caso de no haber encontrado el curso a
+     *             actualizar
+     */
+    public CourseBean update(Long id, CourseBean course);
 
-	/**
-	 * Buscar todos los cursos
-	 * 
-	 * @return lista de todos los cursos
-	 */
-	public List<CourseBean> findAll();
-	
-	/**
-	 * Bucar un curso
-	 * 
-	 * @param id - identificador del curso a buscar
-	 * @return curso buscado
-	 * @throws EntityNotFoundException Excepción en caso de no haber encontrado el curso
-	 */
-	public CourseBean findOne(Long id) throws InstitucionesPublicasServiceException;
+    /**
+     * Elimina un curso
+     * 
+     * @param id
+     *            - identificación del curso a eliminar
+     * @throws EntityNotFoundException
+     *             Excepción en caso de no haber encontrado el curso a eliminar
+     */
+    public void delete(Long id);
+
+    /**
+     * Buscar todos los cursos
+     * 
+     * @return lista de todos los cursos
+     */
+    public List<CourseBean> findAll();
+
+    /**
+     * Bucar un curso
+     * 
+     * @param id
+     *            - identificador del curso a buscar
+     * @return curso buscado
+     * @throws EntityNotFoundException
+     *             Excepción en caso de no haber encontrado el curso
+     */
+    public CourseBean findOne(Long id);
 
 }
--- instituciones-service/src/main/java/es/ing/instituciones/service/impl/CourseServiceImpl.java
+++ instituciones-service/src/main/java/es/ing/instituciones/service/impl/CourseServiceImpl.java
@@ -13,62 +13,61 @@ import es.ing.instituciones.service.ICourseService;
 
 /**
  * Implementación de la interfaz {@link ICourseService}
- * 
+ *
  * @author daniel.diaz
  *
  */
 @Service
-public class CourseServiceImpl implements ICourseService{
+public class CourseServiceImpl implements ICourseService {
 
-	@Autowired
-	private ICourseRepository courseMapper;
-//
-//	public CourseServiceImpl(final ICourseMapper courseMapper) {
-//		this.courseMapper = courseMapper;
-//	}
+    @Autowired
+    private ICourseRepository courseMapper;
 
-	@Override
-	public CourseBean create(CourseBean course) {
-		this.courseMapper.insert(course);
-		return course;
-	}
+    @Override
+    public CourseBean create(CourseBean course) {
+        courseMapper.insert(course);
+        return course;
+    }
 
-	@Override
-	public CourseBean update(Long id, CourseBean course) throws InstitucionesPublicasServiceException {
-		CourseBean courseBD = this.courseMapper.getById(id);
-		if (courseBD == null) {
-			throw new InstitucionesPublicasServiceException(null, HttpStatus.PRECONDITION_FAILED,"The course to update does not exists");
-		}
-		courseBD.setTitle(course.getTitle());
-		courseBD.setLevel(course.getLevel());
-		courseBD.setNumberOfHours(course.getNumberOfHours());
-		courseBD.setTeacher(course.getTeacher());
-		courseBD.setState(course.getState());
+    @Override
+    public CourseBean update(Long id, CourseBean course) {
+        final CourseBean courseBD = courseMapper.getById(id);
+        if (courseBD == null) {
+            throw new InstitucionesPublicasServiceException(null, HttpStatus.PRECONDITION_FAILED,
+                    "The course to update does not exists");
+        }
+        courseBD.setTitle(course.getTitle());
+        courseBD.setLevel(course.getLevel());
+        courseBD.setNumberOfHours(course.getNumberOfHours());
+        courseBD.setTeacher(course.getTeacher());
+        courseBD.setState(course.getState());
 
-		this.courseMapper.update(courseBD);
-		return courseBD;
-	}
+        courseMapper.update(courseBD);
+        return courseBD;
+    }
 
-	@Override
-	public void delete(Long id) throws InstitucionesPublicasServiceException {
-		if (this.courseMapper.getById(id) == null) {
-			throw new InstitucionesPublicasServiceException(null, HttpStatus.PRECONDITION_FAILED,"The course to delete does not exists");
-		}
-		this.courseMapper.deleteById(id);
-	}
+    @Override
+    public void delete(Long id) {
+        if (courseMapper.getById(id) == null) {
+            throw new InstitucionesPublicasServiceException(null, HttpStatus.PRECONDITION_FAILED,
+                    "The course to delete does not exists");
+        }
+        courseMapper.deleteById(id);
+    }
 
-	@Override
-	public List<CourseBean> findAll() {
-		return this.courseMapper.getAll();
-	}
+    @Override
+    public List<CourseBean> findAll() {
+        return courseMapper.getAll();
+    }
 
-	@Override
-	public CourseBean findOne(Long id) throws InstitucionesPublicasServiceException {
-		CourseBean course = this.courseMapper.getById(id);
-		if (course == null) {
-			throw new InstitucionesPublicasServiceException(null, HttpStatus.PRECONDITION_FAILED,"The course does not exists");
-		}
-		return course;
-	}
+    @Override
+    public CourseBean findOne(Long id) {
+        final CourseBean course = courseMapper.getById(id);
+        if (course == null) {
+            throw new InstitucionesPublicasServiceException(null, HttpStatus.PRECONDITION_FAILED,
+                    "The course does not exists");
+        }
+        return course;
+    }
 
 }
